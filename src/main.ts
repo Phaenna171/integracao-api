85c2d1c5345dcd967fd40793f227ed38c96ea89e
Aenewsss <aenamartinelli@gmail.com>
2024-10-09 21:14:49 -0300

    ok

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,7 @@ import { initializeFirebaseApp } from "./firebase";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://safrasul-sementes.vercel.app',
-      'https://safrasul-mu.vercel.app',
-      'https://safrasulsementes.com.br',
-    ]
-  })
+  app.enableCors()
 
   initializeFirebaseApp()
 
